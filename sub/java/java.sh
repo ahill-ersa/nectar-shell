f398038636030fd635b59162a387c2de8094f559
Craig Patten <cjp@cjp.id.au>
2014-12-16 15:40:58 +1030

    Install java on ZFS.

--- sub/java/java.sh
+++ sub/java/java.sh
@@ -5,11 +5,15 @@ if [ -z "$JAVA_URL" ] ; then
   exit 0
 fi
 
+fs=data/java
+installdir=/$fs
+
 aria2c -o javatmp.$$ --file-allocation=falloc -x 4 $JAVA_URL
-tar xf javatmp.$$ -C /usr/local
+zfs create $fs
+tar xf javatmp.$$ -C $installdir
 rm -f javatmp.$$
 
-for executable in /usr/local/java*/bin/* ; do
+for executable in $installdir/java*/bin/* ; do
     execname=`basename $executable`
     update-alternatives --install /usr/bin/$execname $execname $executable 10
 done
