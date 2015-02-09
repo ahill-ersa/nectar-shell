8e679fc8c99d3dcfb0b7bcc0fa8bee9c02cee240
Shunde Zhang <shunde.p.zhang@gmail.com>
2015-02-09 15:49:07 +1030

    added search config to settings.py

--- sub/mytardis-prep/install.sh
+++ sub/mytardis-prep/install.sh
@@ -2,13 +2,6 @@
 
 export HOSTNAME=`hostname`
 
-# install elasticsearch
-wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo add-apt-repository "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main"
-sudo apt-get update && sudo apt-get install elasticsearch
-sudo update-rc.d elasticsearch defaults 95 10
-sudo /etc/init.d/elasticsearch start
-
 top=/data/mytardis
 
 cd $top
@@ -20,6 +13,30 @@ cd mytardis
 git checkout `cat $top/checkout.txt`
 
 cp $top/settings.py tardis/settings.py
+
+grep search $top/checkout.txt
+if [ $? == 0 ]; then
+  # install elasticsearch
+  wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+  sudo add-apt-repository "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main"
+  sudo apt-get update && sudo apt-get install elasticsearch
+  sudo update-rc.d elasticsearch defaults 95 10
+  sudo /etc/init.d/elasticsearch start
+
+  # update settings for new haystack
+  sed -i '/^SINGLE_SEARCH_ENABLED/SINGLE_SEARCH_ENABLED = True/' tardis/settings.py
+  sed -i '/^HAYSTACK_SITECONF/s/^/# /' tardis/settings.py
+  sed -i '/^HAYSTACK_SEARCH_ENGINE/s/^/# /' tardis/settings.py
+  sed -i '/^HAYSTACK_SOLR_URL/s/^/# /' tardis/settings.py
+  sed -i '/haystack/{n;s/^/#/;n;s/^/#/;}' tardis/settings.py
+  sed -i "/HAYSTACK_ENABLE_REGISTRATIONS/aHAYSTACK_CONNECTIONS = {\n\
+    'default': {\n\
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',\n\
+        'URL': 'http://127.0.0.1:9200/',\n\
+        'INDEX_NAME': 'haystack',\n\
+    },\n\
+}" tardis/settings.py
+fi
 
 cat > buildout-dev.cfg << EOF
 [buildout]
