2a876f7bd13db65d9cbea557ac67ef0937e5d5ce
Shunde Zhang <shunde.p.zhang@gmail.com>
2015-02-09 12:00:19 +1030

    Update install.sh

--- sub/mytardis-prep/install.sh
+++ sub/mytardis-prep/install.sh
@@ -2,6 +2,13 @@
 
 export HOSTNAME=`hostname`
 
+# install elasticsearch
+wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo add-apt-repository "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main"
+sudo apt-get update && sudo apt-get install elasticsearch
+sudo update-rc.d elasticsearch defaults 95 10
+sudo /etc/init.d/elasticsearch start
+
 top=/data/mytardis
 
 cd $top
