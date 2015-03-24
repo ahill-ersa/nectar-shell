077be670f3cde8bd225f587aaefc926cf5488390
Craig Patten <cjp@cjp.id.au>
2015-03-24 14:50:02 +1030

    workaround for flaky resolvers.

--- sub/ddns/ddns.sh
+++ sub/ddns/ddns.sh
@@ -7,6 +7,13 @@ fi
 
 export DDNS_HOSTNAME=`curl --silent http://169.254.169.254/latest/meta-data/hostname | cut -f1 -d.`
 
+### workaround for flaky resolvers
+### remove when this (or at least a switch for this) is built into base image
+apt-get install bind9
+echo "nameserver 127.0.0.1" > /etc/resolv.conf
+echo "supersede domain-name-servers 127.0.0.1;" >> /etc/dhcp/dhclient.conf
+###
+
 ddns=/etc/cron.hourly/ddns
 
 ../../render.py < ddns > $ddns
