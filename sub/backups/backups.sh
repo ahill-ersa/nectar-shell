df09cbe37300dad830c895763f3eec3593df6857
Craig Patten <cjp@cjp.id.au>
2015-05-26 16:24:37 +0930

    backup parameterisation

--- sub/backups/backups.sh
+++ sub/backups/backups.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -e
 
+if [ ! $OAGR_BACKUP == "YES" ] ; then
+    exit 0
+fi
+
 top=/data/mytardis
 
 install -o ubuntu -g ubuntu -m 755 backup.?? $top
 
-echo '42 * * * * '$top'/backup.sh' | su -l ubuntu -c crontab
+let minute="$RANDOM % 60"
+
+echo $minute' * * * * '$top'/backup.sh' | su -l ubuntu -c crontab
