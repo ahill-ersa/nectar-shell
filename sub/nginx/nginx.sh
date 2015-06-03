baced32ef619ec4ab72dde288ecb9a55b50bdb66
Craig Patten <cjp@cjp.id.au>
2015-06-03 16:49:50 +0930

    Relocate nginx buffer.

--- sub/nginx/nginx.sh
+++ sub/nginx/nginx.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
+fs=data/nginx
+zfs create $fs
+chown www-data:www-data /$fs
+
 ../../render.py < nginx.conf > /etc/nginx/nginx.conf
 
 echo $OAGR_HTTPS_BUNDLE | sed 's/ //g' | base64 --decode | tar xJvf - -C /etc/nginx
