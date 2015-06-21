133ccf48e99d035de3a463d315a2441c95fa3136
Craig Patten <cjp@cjp.id.au>
2015-06-22 01:35:59 +0930

    restore: "latest" or "" → use latest snapshot

--- sub/mytardis-prep/restore.py
+++ sub/mytardis-prep/restore.py
@@ -9,6 +9,10 @@ aws_secret = hashlib.md5(os.getenv("OAGR_BACKUP_SECRET")).hexdigest()
 hs3 = S3Connection(aws_access_key_id=aws_id, aws_secret_access_key=aws_secret, host=os.getenv("OAGR_BACKUP_HOST"))
 bucket = hs3.get_bucket(os.getenv("OAGR_BACKUP_BUCKET"))
 
-data = bucket.get_key(os.getenv("OAGR_RESTORE")).read()
+name = os.getenv("OAGR_RESTORE")
+if not name or name in [ "", "latest" ]:
+    name = sorted([item.name for item in bucket.get_all_keys()])[-1]
+
+data = bucket.get_key(name).read()
 
 sys.stdout.write(data)
